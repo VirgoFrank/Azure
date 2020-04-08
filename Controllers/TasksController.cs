fd707dca2a0c2f6a39c2158b06de2dd9bbd16c13
Virgo Frank <Virgo.Frank@tptlive.ee>
2020-04-08 11:26:24 +0300

    first commit

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Data.Migrations;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tasks>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+        [HttpPost("{get-tasks}")]
+        public async Task<ActionResult<IEnumerable<Tasks>>> GetTasks(Users users)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(i => i.token == users.token);
+
+            var tasks = await _context.Tasks.Where(i => i.UserId == user.Id.ToString()).ToListAsync();
+
+            return tasks;
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tasks>> GetTasks(int id)
+        {
+            var tasks = await _context.Tasks.FindAsync(id);
+
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            return tasks;
+        }
+
+        // PUT: api/Tasks/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTasks(int id, Tasks tasks)
+        {
+            if (id != tasks.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tasks).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TasksExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Tasks>> PostTasks(Users user)
+        {
+            var currentUser = await _context.Users.FirstOrDefaultAsync
+                                     (i => i.token == user.token);
+
+            Tasks tasks = new Tasks();
+            tasks.UserId = currentUser.Id.ToString();
+
+            _context.Tasks.Add(tasks);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTasks", new { id = tasks.Id }, tasks);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Tasks>> DeleteTasks(int id)
+        {
+            var tasks = await _context.Tasks.FindAsync(id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(tasks);
+            await _context.SaveChangesAsync();
+
+            return tasks;
+        }
+
+        private bool TasksExists(int id)
+        {
+            return _context.Tasks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Data.Migrations;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Users>> Register(Users users)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+
+            users.token = new String(stringChars);
+            _context.Users.Add(users);
+            await _context.SaveChangesAsync();
+
+            return await _context.Users.FindAsync(users.Id);
+        }
+
+        [HttpPost("{get-token}")]
+        public async Task<ActionResult<string>> Get_Token(Users users)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(i => i.Username == users.Username && i.Password == users.Password);
+
+            return user.token;
+        }
+       
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return users;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsers(int id, Users users)
+        {
+            if (id != users.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPost]
+        //public async Task<ActionResult<Users>> PostUsers(Users users)
+        //{
+        //    _context.Users.Add(users);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetUsers", new { id = users.Id }, users);
+        //}
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Users>> DeleteUsers(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(users);
+            await _context.SaveChangesAsync();
+
+            return users;
+        }
+
+        private bool UsersExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
